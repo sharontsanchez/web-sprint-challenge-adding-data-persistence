7b08fe9d32fc15039795727169a833dd69907f8b
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-18 13:22:58 -0600

    installed dependencies, scripts written and set up the server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res, next) => { // eslint-disable-line
+    res.send('server is up and running!')
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err. status|| 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = server
