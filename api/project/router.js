99783fdd0f057723e84c5f5a2e25b805b359eb6d
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-18 14:40:31 -0600

    the routers finished for project, resource and task

--- api/project/router.js
+++ api/project/router.js
@@ -2,4 +2,23 @@
 const router = require('express').Router()
 const Project = require('./model')
 
+
+router.get('/', (req, res, next) => {
+    Project.getProjects()
+      .then((projects) => {
+        res.json(projects)
+      })
+      .catch(next)
+  })
+  
+  router.post('/', async (req, res, next) => {
+    try {
+      const project = await Project
+        .insert(req.body)
+      res.status(201).json(project)
+    } catch (err) {
+      next(err)
+    }
+  })
+
 module.exports = router
