033ec719edb2bb86b06e9a60d5347d284cf81be0
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-18 14:50:47 -0600

    tweaked code and changed the version of knex

--- api/server.js
+++ api/server.js
@@ -8,9 +8,16 @@ const server = express()
 
 server.use(express.json())
 
-server.get('/', (req, res, next) => { // eslint-disable-line
-    res.send('server is up and running!')
-})
+server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
+
+server.use('*', (req, res, next) => {
+    next({
+      status: 404,
+      message: 'This endpoint does not exist'
+    })
+  })
 
 server.use((err, req, res, next) => { // eslint-disable-line
     res.status(err. status|| 500).json({
