97195b6a03f797fdc38e06eeed233da1de6b7265
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-18 14:30:41 -0600

    project model done

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,33 @@
 // build your `Project` model here
 const db = require('./../../data/dbConfig')
+
+async function getProjects() {
+    const projects = await db('projects')
+    return projects.map((project) => {
+      return {
+        ...project,
+        project_completed: project.project_completed === 1,
+      }
+    })
+  }
+  
+  async function findById (id) {
+    const row = await db('projects')
+        .where('project_id', id)
+        .first();
+    return {
+        ...row, 
+        project_completed: row.project_completed ? true : false
+    }
+  }
+  
+  async function insert (project) {
+    const [id] = await db('projects')
+        .insert(project)
+    return findById(id)
+  }
+  
+  module.exports = {
+    getProjects,
+    insert
+  }
